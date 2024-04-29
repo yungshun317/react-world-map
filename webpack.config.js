f08ddb3731654e7dee5a040b115dc4a99fa7f877
yungshun317 <seraph317@gmail.com>
2024-04-30 06:05:31 +0800

    [FEAT] Create `index.html` and configure the development build with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
-let cssConfig;
-let pages;
+const currentTask = process.env.npm_lifecycle_event;
+const path = require("path");
+// let cssConfig;
+// let pages;
 
 let config = {
     entry: "./app/assets/scripts/App.js",
-    plugins: pages,
+    // plugins: pages,
     module: {
         rules: [
-            cssConfig,
+            // cssConfig,
             {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
@@ -19,6 +21,25 @@ let config = {
             }
         ]
     }
+}
+
+if (currentTask === "dev") {
+    config.output = {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "app")
+    };
+
+    config.devServer = {
+        watchFiles: [ "app/**/*.html" ],
+        static: {
+            directory: path.join(__dirname, "app"),
+            watch: false
+        },
+        hot: true,
+        port: 3000
+    }
+
+    config.mode = "development";
 }
 
 module.exports = config;
