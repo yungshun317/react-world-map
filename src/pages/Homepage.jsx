88af09522ff59a7b84dd1e05df149927c561b0f4
yungshun317 <seraph317@gmail.com>
2024-05-03 16:42:33 +0800

    [FEAT] Use the `Link` component provided by React Router

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,15 @@
+import {Link} from "react-router-dom";
+
 function Homepage() {
     return (
-        <div>React World Map</div>
+        <div>
+            <h1>React World Map</h1>
+
+            { /* This will reload the page
+                <a href="/pricing">Pricing</a>
+            */ }
+            <Link to="/pricing">Pricing</Link>
+        </div>
     );
 }
 
