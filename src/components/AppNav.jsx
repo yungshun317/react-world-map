26c55c93b12ab57e6af4baa3e8c5583595543492
yungshun317 <seraph317@gmail.com>
2024-05-06 07:09:38 +0800

    [FEAT] Add links to the `AppNav` component

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,9 +1,17 @@
 import * as styles from "./AppNav.module.css";
+import {NavLink} from "react-router-dom";
 
 function AppNav() {
     return (
         <nav className={styles.nav}>
-            App Navigation
+            <ul>
+                <li>
+                    <NavLink to="cities">Cities</NavLink>
+                </li>
+                <li>
+                    <NavLink to="countries">Countries</NavLink>
+                </li>
+            </ul>
         </nav>
     );
 }
