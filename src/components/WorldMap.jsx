c47ea78c4291137a24e47d5b61597532465e08b6
yungshun317 <seraph317@gmail.com>
2024-05-11 22:08:07 +0800

    [FEAT] Interact with the map using `useNavigate`

--- src/components/WorldMap.jsx
+++ src/components/WorldMap.jsx
@@ -1,11 +1,10 @@
 import * as styles from "./WorldMap.module.css";
 import {useNavigate, useSearchParams} from "react-router-dom";
-import {MapContainer, TileLayer, Marker, Popup, useMap} from "react-leaflet";
+import {MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents} from "react-leaflet";
 import {useEffect, useState} from "react";
 import {useCities} from "../contexts/CitiesContext";
 
 function WorldMap() {
-    const navigate = useNavigate();
     const { cities } = useCities();
     const [mapPosition, setMapPosition] = useState([40, 0]);
 
@@ -37,6 +36,7 @@ function WorldMap() {
                     </Marker>
                 )}
                 <ChangeCenter position={mapPosition} />
+                <DetectClick />
             </MapContainer>
         </div>
     );
@@ -46,6 +46,28 @@ function ChangeCenter({position}) {
     const map = useMap();
     map.setView(position);
     return null;
+}
+
+function DetectClick() {
+    const navigate = useNavigate();
+
+    useMapEvents({
+        click: e => {
+            console.log(e);
+            /*
+            {originalEvent: PointerEvent, containerPoint: Point, layerPoint: Point, latlng: LatLng, type: 'click', ...}
+                containerPoint: Point {x: 281, y: 475}
+                latlng: LatLng {lat: 40.697299008636755, lng: -1.3183593750000002}
+                layerPoint: Point {x: 281, y: 475}
+                originalEvent: PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, ...}
+                sourceTarget: NewClass {options: {...}, _handlers: Array(7), _layers: {...}, _zoomBoundLayers: {...}, _sizeChanged: false, ...}
+                target: NewClass {options: {...}, _handlers: Array(7), _layers: {...}, _zoomBoundLayers: {...}, _sizeChanged: false, ...}
+                type: "click"
+                [[Prototype]]: Object
+            */
+            navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+        }
+    });
 }
 
 export default WorldMap;
