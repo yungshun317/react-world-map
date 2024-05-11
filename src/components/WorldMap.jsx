b8272e7ed6ec2a1ed93893e18a47e6222fb7f18c
yungshun317 <seraph317@gmail.com>
2024-05-11 21:57:36 +0800

    [FEAT] Recenter the map with `useEffect`

--- src/components/WorldMap.jsx
+++ src/components/WorldMap.jsx
@@ -1,21 +1,30 @@
 import * as styles from "./WorldMap.module.css";
 import {useNavigate, useSearchParams} from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import {useState} from "react";
+import {MapContainer, TileLayer, Marker, Popup, useMap} from "react-leaflet";
+import {useEffect, useState} from "react";
 import {useCities} from "../contexts/CitiesContext";
 
 function WorldMap() {
     const navigate = useNavigate();
     const { cities } = useCities();
-    const [mapPosition, setMapPosition] = useState([23.6978, 120.9605]);
+    const [mapPosition, setMapPosition] = useState([40, 0]);
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    const lat = searchParams.get("lat");
-    const lng = searchParams.get("lng");
+    const [searchParams] = useSearchParams();
+    const mapLat = searchParams.get("lat");
+    const mapLng = searchParams.get("lng");
+
+    useEffect(function() {
+        if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    }, [mapLat, mapLng]);
 
     return (
         <div className={styles.mapContainer}>
-            <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+            <MapContainer
+                className={styles.map}
+                center={mapPosition}
+                zoom={6}
+                scrollWheelZoom={true}
+            >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
@@ -27,9 +36,16 @@ function WorldMap() {
                         </Popup>
                     </Marker>
                 )}
+                <ChangeCenter position={mapPosition} />
             </MapContainer>
         </div>
     );
+}
+
+function ChangeCenter({position}) {
+    const map = useMap();
+    map.setView(position);
+    return null;
 }
 
 export default WorldMap;
